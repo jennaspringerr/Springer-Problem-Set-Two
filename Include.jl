e8a118ac9b66a462ab21c6ec2637b4d21ef7e202
Jenna <jjs489@cornell.edu>
2023-03-12 22:13:08 -0400

    hi

---
+++ Include.jl
@@ -0,0 +1,10 @@
+# setup paths -
+const _ROOT = pwd()
+const _PATH_TO_SRC = joinpath(_ROOT, "src")
+const _PATH_TO_DATA = joinpath(_ROOT, "data")
+
+# load external packages -
+# TODO: Fill in any external requirements 
+
+# load my codes -
+# TODO: Fill in the include statements with your files
